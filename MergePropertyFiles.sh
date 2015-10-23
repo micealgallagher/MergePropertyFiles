af31ada68858f0ba076e1cc0691dfce5fa32759f
Miceal Gallagher <me@mehaul.me>
2015-10-23 11:55:52 -0400

    Update property/value detection
     - Ignore lines that begin with the # symbol since they are comments
     - Properties that don't have values should be preserved as such

--- MergePropertyFiles.sh
+++ MergePropertyFiles.sh
@@ -1,22 +1,51 @@
 #!/bin/bash
-MASTER_FILE="./master.properties"
-SLAVE_FILE="./slave.properties"
-OUTPUT_FILE="./output.properties"
+MASTER_FILE="$1"
+SLAVE_FILE="$2"
+OUTPUT_FILE="$3"
 
 # Delete the output file
 rm -rf "$OUTPUT_FILE"
 
+
+# Ensure files exist before attempting to merge
+if [ ! -e $MASTER_FILE ] ; then
+    echo 'Unable to merge property files: MASTER_FILE doesn''t exist'
+    exit
+fi
+
+if [ ! -e $SLAVE_FILE ] ; then
+    echo 'Unable to merge property files: SLAVE_FILE doesn''t exist'
+    exit
+fi
+
+# Read property files into arrays
 readarray MASTER_FILE < "$MASTER_FILE"
 readarray SLAVE_FILE < "$SLAVE_FILE"
+
+# Regex strings to check for values and 
+COMMENT_LINE_REGEX="[#*]"
+HAS_VALUE_REGEX="[*=*]"
 
 declare -A ALL_PROPERTIES
 
 # All the master file property names and values will be preserved
 for MASTER_FILE_LINE in "${MASTER_FILE[@]}"; do
+
     MASTER_PROPERTY_NAME=`echo $MASTER_FILE_LINE | cut -d = -f1`
-    MASTER_PROPERTY_VALUE=`echo $MASTER_FILE_LINE | cut -d = -f2-`
+
+    # Only attempt to get the property value if it exists
+    if [[ $MASTER_FILE_LINE =~ $HAS_VALUE_REGEX ]]
+    then
+        MASTER_PROPERTY_VALUE=`echo $MASTER_FILE_LINE | cut -d = -f2-`
+    else
+        MASTER_PROPERTY_VALUE=''
+    fi
     
-    ALL_PROPERTIES[$MASTER_PROPERTY_NAME]=$MASTER_PROPERTY_VALUE
+    # Ignore the line if it begins with the # symbol as it is a comment
+    if ! [[ $MASTER_PROPERTY_NAME =~ $COMMENT_LINE_REGEX ]]
+    then
+        ALL_PROPERTIES[$MASTER_PROPERTY_NAME]=$MASTER_PROPERTY_VALUE
+    fi
 done
 
 # Properties that are in the slave but not the master will be preserved
@@ -24,13 +53,25 @@ for SLAVE_FILE_LINE in "${SLAVE_FILE[@]}"; do
     SLAVE_PROPERTY_NAME=`echo $SLAVE_FILE_LINE | cut -d = -f1`
     SLAVE_PROPERTY_VALUE=`echo $SLAVE_FILE_LINE | cut -d = -f2-`
     
+    # Only attempt to get the property value if it exists
+    if [[ $MASTER_FILE_LINE =~ $HAS_VALUE_REGEX ]]
+    then
+        MASTER_PROPERTY_VALUE=`echo $MASTER_FILE_LINE | cut -d = -f2-`
+    else
+        MASTER_PROPERTY_VALUE=''
+    fi
+
     # If a slave property exists in the master, the master's value will be used preserved
-    if [ ! ${ALL_PROPERTIES[$SLAVE_PROPERTY_NAME]+_} ]; 
-    then 
-        ALL_PROPERTIES[$SLAVE_PROPERTY_NAME]=$SLAVE_PROPERTY_VALUE;
+    if [ ! ${ALL_PROPERTIES[$SLAVE_PROPERTY_NAME]+_ } ]; 
+    then
+        # If the line begins with a # symbol it is a comment line and should be ignored
+        if ! [[ $SLAVE_PROPERTY_NAME =~ $COMMENT_LINE_REGEX ]]
+        then
+            ALL_PROPERTIES[$SLAVE_PROPERTY_NAME]=$SLAVE_PROPERTY_VALUE
+        fi
     fi
 done
 
-for KEY in "${!ALL_PROPERTIES[@]}"; do 
+for KEY in "${!ALL_PROPERTIES[@]}"; do
     echo "$KEY=${ALL_PROPERTIES[$KEY]}" >> "$OUTPUT_FILE"
 done
